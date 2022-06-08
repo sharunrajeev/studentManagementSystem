2d52ed81bd595bc701f4be57911d64d3ca8a90cf
sharun <sharunpublic@gmai.com>
2022-06-08 20:40:27 +0530

    Update requiments.txt file with latest packages. Change read_dotenv() to load_dotenv() - bug fix

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
+    dotenv.load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentManagementSystem.settings')
     try:
         from django.core.management import execute_from_command_line
